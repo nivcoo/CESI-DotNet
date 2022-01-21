1c9ff872f2639b3b3377d87e8f90e31380b55ce1
nivcoo <nico64800@live.fr>
2022-01-21 11:13:56 +0100

    improv. add log

---
+++ MainApplication/Objects/Log.cs
@@ -0,0 +1,24 @@
+﻿using MainApplication.Objects.Enums;
+
+namespace MainApplication.Objects;
+
+public class Log
+{
+    public string? Name { get; set; }
+    public string? SourcePath { get; set; }
+    public string? TargetPath { get; set; }
+
+    public int? FileSize { get; set; }
+    public double? FileTransferTime { get; set; }
+    public DateTime? Time { get; set; }
+
+    public Log(string name, string sourcePath, string targetPath, int fileSize, double fileTransferTime, DateTime time)
+    {
+        Name = name;
+        SourcePath = sourcePath;
+        TargetPath = targetPath;
+        FileSize = fileSize;
+        FileTransferTime = fileTransferTime;
+        Time = time;
+    }
+}
