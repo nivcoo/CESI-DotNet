66e3d1c6365b41a6108c51db3cf6cc80234ec58f
nivcoo <nico64800@live.fr>
2022-01-21 08:50:48 +0100

    feat. add implemented methods

--- MainApplication/Storage/IStorage.cs
+++ MainApplication/Storage/IStorage.cs
@@ -2,5 +2,9 @@
 
 public interface IStorage
 {
-    
+    IDictionary<string, object> GetAllElements();
+
+    void AddNewElement(IDictionary<string, object> objects);
+
+    void EditElementByName(string name, IDictionary<string, object> objects);
 }
--- MainApplication/Storage/JsonStorage.cs
+++ MainApplication/Storage/JsonStorage.cs
@@ -2,5 +2,18 @@
 
 public class JsonStorage : IStorage
 {
-    
+    public IDictionary<string, object> GetAllElements()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void AddNewElement(IDictionary<string, object> objects)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void EditElementByName(string name, IDictionary<string, object> objects)
+    {
+        throw new NotImplementedException();
+    }
 }
