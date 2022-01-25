40e395f92e10d78ed0080d76cf38d21683549218
nivcoo <nico64800@live.fr>
2022-01-25 10:42:37 +0100

    improv. fix issue

---
+++ MainApplication/Storages/Converter/DateTimeConverter.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics;
+using System.Globalization;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace MainApplication.Storages.Converter;
+
+public class DateTimeConverter : JsonConverter<DateTime>
+{
+    public override DateTime Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        return DateTime.Parse(reader.GetString() ?? string.Empty, CultureInfo.InvariantCulture);
+    }
+
+    public override void Write(Utf8JsonWriter writer, DateTime value, JsonSerializerOptions options)
+    {
+        writer.WriteStringValue(value.ToString(CultureInfo.InvariantCulture));
+    }
+}
