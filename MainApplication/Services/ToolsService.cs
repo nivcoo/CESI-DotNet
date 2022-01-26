dbc480608cbe1234bd98019d018a4454f5d82a16
nivcoo <nico64800@live.fr>
2022-01-26 19:21:22 +0100

    improv. add tools service

---
+++ MainApplication/Services/ToolsService.cs
@@ -0,0 +1,50 @@
+﻿using MainApplication.Objects.Enums;
+
+namespace MainApplication.Services;
+
+internal class ToolsService
+{
+    private static readonly ToolsService Instance = new();
+
+    public static ToolsService GetInstance()
+    {
+        return Instance;
+    }
+
+
+    public static Uri? IsValidUri(string? uri)
+    {
+        if (uri == null)
+            return null;
+        try
+        {
+            return new Uri(uri);
+        }
+        catch (Exception)
+        {
+            return null;
+        }
+    }
+
+    public static T? IsValidEnumInteger<T>(int obj)
+    {
+        if (Enum.IsDefined(typeof(T), obj))
+            return (T) Enum.Parse(typeof(T), obj.ToString());
+        return default;
+    }
+
+    public static T? ConvertStringIntegerToEnum<T>(string? choiceString)
+    {
+        if (choiceString == null)
+            return default;
+        try
+        {
+            var convertedChoice = Convert.ToInt32(choiceString);
+            return IsValidEnumInteger<T>(convertedChoice);
+        }
+        catch (FormatException)
+        {
+            return default;
+        }
+    }
+}
