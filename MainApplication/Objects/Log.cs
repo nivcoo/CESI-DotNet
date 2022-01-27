de179683d7ed78ffae6f2d47a20bd981d41b380e
nivcoo <nico64800@live.fr>
2022-01-27 17:42:06 +0100

    improv. remove useless code

--- MainApplication/Objects/Log.cs
+++ MainApplication/Objects/Log.cs
@@ -5,17 +5,15 @@ namespace MainApplication.Objects;
 
 public class Log
 {
-    [JsonPropertyName("Name")] public string Name { get; set; }
-    [JsonPropertyName("SourcePath")] public Uri SourcePath { get; set; }
-    [JsonPropertyName("TargetPath")] public Uri TargetPath { get; set; }
-    [JsonPropertyName("FileSize")] public long FileSize { get; set; }
-    [JsonPropertyName("FileTransferTime")] long FileTransferTime { get; set; }
+    public string Name { get; set; }
+    public Uri SourcePath { get; set; }
+    public Uri TargetPath { get; set; }
+    public long FileSize { get; set; }
+    long FileTransferTime { get; set; }
 
-    [JsonPropertyName("Date")]
     [JsonConverter(typeof(DateTimeConverter))]
     public DateTime Date { get; set; }
 
-    [JsonConstructor]
     public Log(string name, Uri sourcePath, Uri targetPath, long fileSize, long fileTransferTime, DateTime date)
     {
         Name = name;
