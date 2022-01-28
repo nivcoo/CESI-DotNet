7e8244c30b84f056b6cb748fc043a22dcb27406c
AC <alexis.canteli@viacesi.fr>
2022-01-28 09:59:07 +0100

    Correction on the previous commit

--- MainApplication/Localization/LanguageCheck.cs
+++ MainApplication/Localization/LanguageCheck.cs
@@ -11,15 +11,18 @@ public abstract class LanguageCheck
             _ => false,
         };
     }
-}
+
 // Mettre en place la gestion auto des langues --> si langue clavier = Fr alors appli Fr sinon EN
 // Ici le code en exemple 
-//public System.Globalization.CultureInfo Culture { get; }
-//public void MyCulture() {
+public System.Globalization.CultureInfo Culture { get; set }
+
+public void MyCulture() {
    // Gets the current input language.
-   //InputLanguage myCurrentLanguage = InputLanguage.CurrentInputLanguage;
+InputLanguage myCurrentLanguage = InputLanguage.CurrentInputLanguage;
 
    // Gets the culture for the language  and prints it.
-  // CultureInfo myCultureInfo = myCurrentLanguage.Culture;
-  // textBox1.Text = myCultureInfo.EnglishName;
-//}
+   CultureInfo myCultureInfo = myCurrentLanguage.Culture;
+  Console.WriteLine = (myCultureInfo.EnglishName);
+}
+
+}
