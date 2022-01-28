0eb59849070a10d9c4bf5427a5a60f8b2e898a26
erouins <p.hugo91@orange.fr>
2022-01-28 13:27:46 +0100

    Differentialsave done

--- MainApplication/Services/Saves/DifferentialSave.cs
+++ MainApplication/Services/Saves/DifferentialSave.cs
@@ -12,7 +12,31 @@ public class DifferentialSave : ASave
 
     protected override bool RetrieveFilesToCopy()
     {
-        throw new NotImplementedException();
+        var files = GetAllFolderFiles(Save.SourcePath);
+        
+        if (files.Length <= 0)
+            return false;
+        var localsourcepath = Save.SourcePath.LocalPath;
+        var localtargetpath = Save.TargetPath.LocalPath;
+        
+        foreach (var file in files)
+        {
+            var filefolder = file.Replace(localsourcepath, "");
+            var path = Path.GetDirectoryName(file);
+            if (path == null)
+                continue;
+            var fileName = Path.GetFileName(file);
+            var hash = GetHashSha256(file);
+            var targetfile = localtargetpath+ filefolder;
+            if (!IsSameFile(hash, GetHashSha256(targetfile)))
+            {
+                Console.WriteLine(file);
+                    var sourceFileInfo = new FileInfo(file);
+                SaveFiles.Add(new SaveFile(path, fileName, hash, sourceFileInfo.Length));
+            }
+        }
+
+        return true;
     }
 
     protected override bool CopyFiles()
@@ -21,24 +45,18 @@ public class DifferentialSave : ASave
             return false;
         var sourceLocalPath = Save.SourcePath.LocalPath;
         var targetLocalPath = Save.TargetPath.LocalPath;
-
+        DeleteFolderWithFiles(Save.TargetPath);
         foreach (var saveFile in SaveFiles)
         {
             var actualTimestamp = ToolService.GetTimestamp();
             var sourceFolder = saveFile.Path;
             var localPath = sourceFolder.Replace(sourceLocalPath, "");
             var targetFolder = targetLocalPath + localPath;
-            var fileName = saveFile.FileName;
-
-            var targetFilePath = Path.Combine(targetFolder, fileName);
-
-
-            if (IsSameFile(saveFile.Hash, GetHashSha256(targetFilePath)))
-                continue;
-            File.Delete(targetFilePath);
             Directory.CreateDirectory(targetFolder);
+            var fileName = saveFile.FileName;
             var sourceFilePath = Path.Combine(sourceFolder, fileName);
-            File.Copy(sourceFilePath, targetFilePath);
+            var targetFilePath = Path.Combine(targetFolder, fileName);
+            File.Copy(sourceFilePath, targetFilePath, true);
             UpdateSaveStatut();
             var sourceFileInfo = new FileInfo(sourceFilePath);
             var finalTimestamp = ToolService.GetTimestamp();
