904659c51b80a52584395d0e85f1ef9aa28e4d41
nivcoo <nico64800@live.fr>
2022-02-02 09:13:56 +0100

    feat. add winui gui

---
+++ GuiApplication/Views/Dialogs/CreateSaveDialog.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.UI.Xaml.Controls;
+
+namespace GuiApplication.Views.Dialogs;
+
+public sealed partial class CreateSaveDialog : Page
+{
+    public CreateSaveDialog()
+    {
+        InitializeComponent();
+    }
+}
+
---
+++ GuiApplication/Views/Pages/HomePage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.UI.Xaml.Controls;
+
+namespace GuiApplication.Views.Pages;
+
+public sealed partial class HomePage : Page
+{
+    public HomePage()
+    {
+        InitializeComponent();
+    }
+}
+
