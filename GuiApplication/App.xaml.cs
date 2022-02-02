e4f05251f65e196266ecdf356ebb809185ba5e65
nivcoo <nico64800@live.fr>
2022-02-02 09:13:56 +0100

    feat. add winui gui

---
+++ GuiApplication/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿using GuiApplication.Views;
+using Microsoft.UI.Xaml;
+
+namespace GuiApplication;
+
+public partial class App : Application
+{
+    public App()
+    {
+        this.InitializeComponent();
+    }
+
+    protected override void OnLaunched(LaunchActivatedEventArgs args)
+    {
+        m_window = new MainWindow();
+        m_window.Activate();
+    }
+
+    private Window m_window;
+}
+
---
+++ GuiApplication/Views/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.UI;
+using Microsoft.UI.Windowing;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using System;
+
+namespace GuiApplication.Views;
+
+public sealed partial class MainWindow : Window
+{
+
+    private AppWindow _apw;
+    private OverlappedPresenter _presenter;
+
+    public void GetAppWindowAndPresenter()
+    {
+        var hWnd = WinRT.Interop.WindowNative.GetWindowHandle(this);
+        WindowId myWndId = Win32Interop.GetWindowIdFromWindow(hWnd);
+        _apw = AppWindow.GetFromWindowId(myWndId);
+        _presenter = _apw.Presenter as OverlappedPresenter;
+    }
+    public MainWindow()
+    {
+        InitializeComponent();
+        GetAppWindowAndPresenter();
+        _presenter.IsResizable = false;
+        _apw.Resize(new Windows.Graphics.SizeInt32 { Width = 1400, Height = 800 });
+        _apw.Title = "EasySave";
+        _presenter.IsMaximizable = false;
+    }
+
+
+    private void NavigationView_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+    {
+            
+            var selectedItem = (NavigationViewItem)args.SelectedItem;
+            string selectedItemTag = ((string)selectedItem.Tag);
+            sender.Header = "Page " + selectedItemTag;
+            string pageName = "GuiApplication.Views.Pages." + selectedItemTag;
+            Type pageType = Type.GetType(pageName);
+            MainContentFrame.Navigate(pageType);
+            
+    }
+}
+
