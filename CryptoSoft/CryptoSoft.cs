92fb88086bd1a4d1d56ecd3e53225851f886c6da
erouins <p.hugo91@orange.fr>
2022-02-09 16:10:41 +0100

    CryptoSoft class

--- CryptoSoft/CryptoSoft.cs
+++ CryptoSoft/CryptoSoft.cs
@@ -1,5 +1,26 @@
 ﻿namespace CryptoSoft;
 
+
 public class CryptoSoft
 {
+    public CryptoSoft(string readPath, string writePath, string keyCript)
+    {
+        Encrypt(readPath, writePath, keyCript);
+    }
+    public void Encrypt(string fileToRead, string fileToWrite, string keyCrypt)
+    {
+        var file = System.IO.File.ReadAllText(fileToRead);
+    
+        var count = 0;
+        var textfile = "";
+        foreach (var letter in file.Where(letter => keyCrypt != null))
+        {
+            if (keyCrypt == null) continue;
+            textfile += (char)((int) keyCrypt[count] ^ (int) letter);
+
+            count++;
+            if (count == keyCrypt.Length) count = 0;
+        }
+        System.IO.File.WriteAllText(fileToWrite,textfile);
+    }
 }
