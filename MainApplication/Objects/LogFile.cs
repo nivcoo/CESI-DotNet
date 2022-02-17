ffe6abf8e3c85b6b7117cfbac86538bc2f687b48
nivcoo <nico64800@live.fr>
2022-02-17 10:02:54 +0100

    feat. add log page

---
+++ MainApplication/Objects/LogFile.cs
@@ -0,0 +1,14 @@
+﻿namespace MainApplication.Objects;
+
+public class LogFile
+{
+    public string FileName { get; }
+    public string FilePath { get; }
+
+
+    public LogFile(string fileName, string filePath)
+    {
+        FileName = fileName;
+        FilePath = filePath;
+    }
+}
--- MainApplication/ViewModels/BaseViewModel.cs
+++ MainApplication/ViewModels/BaseViewModel.cs
@@ -12,6 +12,7 @@ public class BaseViewModel : INPChanged
     internal readonly ConfigurationService ConfigurationService = ConfigurationService.GetInstance();
     internal readonly EasySaveService EasySaveService = EasySaveService.GetInstance();
     internal readonly SaveService SaveService = SaveService.GetInstance();
+    internal readonly LogService LogService = LogService.GetInstance();
 
     public static T? ConvertStringIntegerToEnum<T>(string? choiceString)
     {
