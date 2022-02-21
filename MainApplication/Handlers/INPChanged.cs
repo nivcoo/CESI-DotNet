70134f4b3fbaae8a949a5c4f88442f1020545036
nivcoo <nico64800@live.fr>
2022-02-21 16:13:28 +0100

    improv. remove annotations

--- MainApplication/Handlers/INPChanged.cs
+++ MainApplication/Handlers/INPChanged.cs
@@ -1,6 +1,5 @@
 ﻿using System.ComponentModel;
 using System.Runtime.CompilerServices;
-using MainApplication.Annotations;
 
 namespace MainApplication.Handlers;
 
@@ -8,7 +7,6 @@ public abstract class INPChanged : INotifyPropertyChanged
 {
     public event PropertyChangedEventHandler? PropertyChanged;
 
-    [NotifyPropertyChangedInvocator]
     protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
     {
         PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
