727a6f272290823ad5f35d485ded09eaf1da8230
AC <alexis.canteli@viacesi.fr>
2022-02-21 11:13:38 +0100

    improv. XML log

--- MainApplication/Storages/XMLStorage.cs
+++ MainApplication/Storages/XMLStorage.cs
@@ -15,47 +15,111 @@ namespace MainApplication.Storages;
 
         public override GetElement()
         {
-            throw new NotImplementedException();
+              var obj = RunMutexFunc(() =>
+        {
+            var text = File.ReadAllText(FilePath);
+
+            if (text == "")
+                return default;
+                    var elementsList = xmlSerializer.Deserialize<List<T>>(text.Trim(), _serializerOptions);
+
+            return elementsList ?? default;
+        });
+
+        return (T?) obj;
         }
 
         public override List<T> GetAllElements()
         {
-            throw new NotImplementedException();
+            var list = RunMutexFunc(() =>
+        {
+            var text = File.ReadAllText(FilePath);
+            if (text == "")
+                return new List<T>();
+            var elementsList = xmlSerializer.Deserialize<List<T>>(text.Trim(), _serializerOptions);
+            return elementsList ?? new List<T>();
+        }) as List<T>;
+
+        if (list != default)
+            return list;
+        return new List<T>();
         }
 
         public override Void WriteElement(T obj)
         {
-            throw new NotImplementedException();
+             if (obj != null)
+            SerializeAndSaveIntoFiles(obj);
         }
 
         public override Void AddNewElement(T obj)
         {
-            throw new NotImplementedException();
+             var elementsList = GetAllElements();
+        elementsList.Add(obj);
+        SerializeAndSaveIntoFiles(elementsList);
         }
 
         public override Void AddNewElementWithoutRewrite(T obj)
         {
-            throw new NotImplementedException();
+            //voir pout le XML si ca fontionne 
+          /*   RunMutexAction(() =>
+        {
+            var objects = new List<T> {obj};
+            using var fs = new FileStream(FilePath, FileMode.Open);
+            var serializeObject = SerializeObject(objects);
+            using var sw = new StreamWriter(fs);
+            if (fs.Length > 1)
+            {
+                fs.Position = fs.Seek(-1, SeekOrigin.End);
+
+                if (fs.ReadByte() == ']')
+                {
+                    fs.SetLength(fs.Length - 4 - Environment.NewLine.Length);
+                    var serializeObjectWithoutFirst = Regex.Split(serializeObject, Environment.NewLine).Skip(1);
+                    serializeObject = string.Join(Environment.NewLine, serializeObjectWithoutFirst.ToArray());
+                    sw.Write("  }," + Environment.NewLine);
+                }
+            }
+            sw.Write(serializeObject);
+            sw.Close();
+            fs.Close();
+        });*/
         }
 
         public override Void RemoveElement(Predicate<T> match)
         {
-            throw new NotImplementedException();
+                 var elementsList = GetAllElements();
+        var element = elementsList.Find(match);
+        if (element == null)
+            return;
+        elementsList.Remove(element);
+        SerializeAndSaveIntoFiles(elementsList);
         }
 
         public override GetElementBy(Predicate<T> match)
         {
-            throw new NotImplementedException();
+            var elementsList = GetAllElements();
+        return elementsList.Find(match);
         }
 
         public override Boolean EditElementBy(Predicate<T> match, T obj)
         {
-            throw new NotImplementedException();
+              var elementsList = GetAllElements();
+        var selected = elementsList.Find(match);
+        if (selected == null)
+            return false;
+        elementsList.Remove(selected);
+        elementsList.Add(obj);
+        SerializeAndSaveIntoFiles(elementsList);
+        return true;
         }
 
         public override Void ClearFile()
         {
-            throw new NotImplementedException();
+           RunMutexAction(() => File.WriteAllText(FilePath, string.Empty));
         }
+        private string SerializeObject(object obj)
+    {
+        return xmlSerializer.Serialize(obj, _serializerOptions);
+    }
     }
 }
